4d32ded1d4459a776aa7aed988cb98f14d548d4f
epode4 <epode4@gamil.com>
2023-08-25 16:09:25 +0900

    seain

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'index.html', context)
